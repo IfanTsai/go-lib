d75dd5663c322bd3d9e3d4ff85bf6d99e5f47ee6
IfanTsai <i@caiyifan.cn>
2022-02-02 01:08:08 +0800

    add logger which wraps zap

---
+++ logger/logger.go
@@ -0,0 +1,202 @@
+package logger
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	// DefaultLevel the default log level
+	DefaultLevel = zapcore.DebugLevel
+
+	// DefaultTimeLayout the default time layout
+	DefaultTimeLayout = time.RFC3339
+)
+
+// Option custom setup config
+type Option func(*option)
+
+type option struct {
+	level          zapcore.Level
+	fields         map[string]string
+	file           io.Writer
+	timeLayout     string
+	disableConsole bool
+}
+
+// WithDebugLevel only greater than 'level' will output
+func WithDebugLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.DebugLevel
+	}
+}
+
+// WithInfoLevel only greater than 'level' will output
+func WithInfoLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.InfoLevel
+	}
+}
+
+// WithWarnLevel only greater than 'level' will output
+func WithWarnLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.WarnLevel
+	}
+}
+
+// WithErrorLevel only greater than 'level' will output
+func WithErrorLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.ErrorLevel
+	}
+}
+
+// WithField add some field(s) to log
+func WithField(key, value string) Option {
+	return func(opt *option) {
+		opt.fields[key] = value
+	}
+}
+
+// WithIOWriter write log to writer
+func WithIOWriter(writer io.Writer) Option {
+	return func(opt *option) {
+		opt.file = writer
+	}
+}
+
+// WithFilePath write log to some file
+func WithFilePath(filename string) Option {
+	dir := filepath.Dir(filename)
+	if err := os.MkdirAll(dir, 0766); err != nil {
+		panic(err)
+	}
+
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0766)
+	if err != nil {
+		panic(err)
+	}
+
+	return WithIOWriter(zapcore.Lock(f))
+}
+
+// WithFileRotationP write log to some file with rotation
+func WithFileRotationP(filename string) Option {
+	dir := filepath.Dir(filename)
+	if err := os.MkdirAll(dir, 0766); err != nil {
+		panic(err)
+	}
+
+	return WithIOWriter(&lumberjack.Logger{ // concurrent-safed
+		Filename:   filename,
+		MaxSize:    128, // 128 MB
+		MaxBackups: 300,
+		MaxAge:     1, // 1 day
+		LocalTime:  true,
+		Compress:   true,
+	})
+}
+
+// WithTimeLayout custom time format
+func WithTimeLayout(timeLayout string) Option {
+	return func(opt *option) {
+		opt.timeLayout = timeLayout
+	}
+}
+
+// WithDisableConsole WithEnableConsole write log to os.Stdout or os.Stderr
+func WithDisableConsole() Option {
+	return func(opt *option) {
+		opt.disableConsole = true
+	}
+}
+
+// NewJSONLogger return a json-encoder zap logger,
+func NewJSONLogger(opts ...Option) (*zap.Logger, error) {
+	opt := &option{level: DefaultLevel, fields: make(map[string]string)}
+	for _, f := range opts {
+		f(opt)
+	}
+
+	timeLayout := DefaultTimeLayout
+	if opt.timeLayout != "" {
+		timeLayout = opt.timeLayout
+	}
+
+	// similar to zap.NewProductionEncoderConfig()
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger", // used by logger.Named(key); optional; useless
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace", // use by zap.AddStacktrace; optional; useless
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.LowercaseLevelEncoder,
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format(timeLayout))
+		},
+		EncodeDuration: zapcore.MillisDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	jsonEncoder := zapcore.NewJSONEncoder(encoderConfig)
+
+	// lowPriority usd by info\debug\warn
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= opt.level && lvl < zapcore.ErrorLevel
+	})
+
+	// highPriority usd by error\panic\fatal
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= opt.level && lvl >= zapcore.ErrorLevel
+	})
+
+	stdout := zapcore.Lock(os.Stdout) // lock for concurrent safe
+	stderr := zapcore.Lock(os.Stderr) // lock for concurrent safe
+
+	core := zapcore.NewTee()
+
+	if !opt.disableConsole {
+		core = zapcore.NewTee(
+			zapcore.NewCore(jsonEncoder,
+				zapcore.NewMultiWriteSyncer(stdout),
+				lowPriority,
+			),
+			zapcore.NewCore(jsonEncoder,
+				zapcore.NewMultiWriteSyncer(stderr),
+				highPriority,
+			),
+		)
+	}
+
+	if opt.file != nil {
+		core = zapcore.NewTee(core,
+			zapcore.NewCore(jsonEncoder,
+				zapcore.AddSync(opt.file),
+				zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+					return lvl >= opt.level
+				}),
+			),
+		)
+	}
+
+	logger := zap.New(core,
+		zap.AddCaller(),
+		zap.ErrorOutput(stderr),
+	)
+
+	for key, value := range opt.fields {
+		logger = logger.WithOptions(zap.Fields(zapcore.Field{Key: key, Type: zapcore.StringType, String: value}))
+	}
+
+	return logger, nil
+}
