465e75f4eedef208626f740c2e37d63fd916d6a7
IfanTsai <i@caiyifan.cn>
2022-03-17 21:52:36 +0800

    add others info in token payload

--- user/token/paseto_maker.go
+++ user/token/paseto_maker.go
@@ -27,18 +27,18 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 }
 
 // CreateToken creates a new token for a specific user id, username and duration.
-func (maker *PasetoMaker) CreateToken(userID int64, username string, duration time.Duration) (string, error) {
+func (maker *PasetoMaker) CreateToken(userID int64, username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(userID, username, duration)
 	if err != nil {
-		return "", errors.WithMessage(err, "failed to new payload")
+		return "", nil, errors.WithMessage(err, "failed to new payload")
 	}
 
 	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to encrypt payload")
+		return "", nil, errors.Wrap(err, "failed to encrypt payload")
 	}
 
-	return token, nil
+	return token, payload, nil
 }
 
 // VerifyToken checks if the token is valid or not.
